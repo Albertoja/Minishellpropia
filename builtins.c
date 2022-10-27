79c21a4f048df84c4599bcfd1b6766e47c215ed6
Alberto <99497451+Albertoja@users.noreply.github.com>
2022-10-27 18:36:10 +0200

    Add files via upload

--- builtins.c
+++ builtins.c
@@ -10,13 +10,21 @@ void	ft_exit(char **str)
 		exit(0);
 	while (str[1][cont])
 	{
-		if (str[1][cont] < '0' || str[1][cont] > '9')
+
+		if (str[1][0] == '-' || str[1][cont] < '0' || str[1][cont] > '9' || ft_strlen(str[1]) >= 19)
 		{
-			printf("minishell: exit: %s: numeric argument required\n", str[1]);
-			exit(255);
+			if (str[1][0] != '-' && str[1][cont] == '-')
+			{
+				printf("minishell: exit: %s: numeric argument required\n", str[1]);
+				exit(255);
+			}
 		}
 		cont++;
 	}
+	if (str[1][0] == '-' && ft_strncmp("-9223372036854775808", str[1], 21) < 0)
+		printf("minishell: exit: %s: numeric argument required\n", str[1]);
+	else if (str[1][0] != '-' && ft_strncmp("9223372036854775807", str[1], 21) < 0)
+		printf("minishell: exit: %s: numeric argument required\n", str[1]);
 	env = ft_atoi(str[1]);
 	exit(env);
 }
